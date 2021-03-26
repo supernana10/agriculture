ba3f213fb0ef30724ed86fdaaf01066631e265bf
Taylo Kefa <taylokefa@gmail.com>
2021-03-26 18:14:14 +0000

    dialogue

---
+++ Assets/LowPolyFarmLite/perso/MouvementRamScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouvementRamScript : MonoBehaviour
+{
+    public Transform player;
+    public Transform playerCam;
+    public float throwForce=10;
+    private bool hashPlayer = false;
+    private bool beingCarried = false;
+    private bool touched= false;
+
+    // Start is called before the first frame update
+  /*   void Start()
+    {
+        
+    } */
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Vector3.Distance(gameObject.transform.position,player.position);
+         if (dist <= 1.9f){
+              hashPlayer = true;
+         }
+         else
+         {
+             hashPlayer = false;
+         }
+         if (hashPlayer && Input.GetKey(KeyCode.E)){
+             GetComponent<Rigidbody>().isKinematic = true;
+             transform.parent =playerCam;
+             beingCarried =true;
+         }
+         if (beingCarried){
+             if(touched){
+                  GetComponent<Rigidbody>().isKinematic = false;
+                  transform.parent =null;
+                  beingCarried =false;
+                  touched= false;
+             }
+             if(Input.GetMouseButtonDown(0)){
+                 GetComponent<Rigidbody>().AddForce(playerCam.forward* throwForce);
+             }
+             else if(Input.GetMouseButtonDown(1)){
+                    GetComponent<Rigidbody>().isKinematic = false;
+                  transform.parent =null;
+                  beingCarried =false; 
+             }
+         }
+    }
+    void OnTriggerEnter() {
+        if(beingCarried){
+            touched = true;  
+        }
+    }
+}
---
+++ Assets/LowPolyFarmLite/perso/PersoAgricultureScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersoAgricultureScript : MonoBehaviour
+{
+    //Variable
+    public float VitesseMarche = 1.5f;
+    public int CoeffRot = 60;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * Input.GetAxis("Horizontal") * CoeffRot * Time.deltaTime);
+
+        // deplacement avec animation
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            // transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * VitesseMarche * Time.deltaTime);
+            anim.SetBool("walk", true);
+        }
+        else
+        {
+            anim.SetBool("walk", false);
+
+        }
+    }
+
+}
